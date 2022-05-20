cc045a99043cad8fc7b586acdb98eb26de5016be
Haytham Salama <haythamasalama@gmail.com>
2022-05-21 02:41:11 +0300

    Create CustomerDAO.java

---
+++ src/dao/CustomerDAO.java
@@ -0,0 +1,93 @@
+package dao;
+
+import database.DatabaseConnection;
+import model.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CustomerDAO {
+
+    public static boolean login(String username , String password) {
+        try {
+            String sql = "SELECT * FROM CUSTOMER WHERE USERNAME=? AND PASSWORD=?";
+            PreparedStatement statement  = DatabaseConnection.getConnection().prepareStatement(sql);
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static boolean insert(Customer customer) {
+        try {
+            String sql = "INSERT INTO CUSTOMER(NAME,USERNAME,PASSWORD,ADDRESS) values(?,?,?,?)";
+            PreparedStatement statement  = DatabaseConnection.getConnection().prepareStatement(sql);
+
+            statement.setString(1, customer.getName());
+            statement.setString(2, customer.getUsername());
+            statement.setString(3, customer.getPassword());
+            statement.setString(4, customer.getAddress());
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static boolean update(Customer customer) {
+        try {
+            String sql = "UPDATE CUSTOMER SET NAME=?,USERNAME=?,ADDRESS=? WHERE ID=?";
+            PreparedStatement statement  = DatabaseConnection.getConnection().prepareStatement(sql);
+
+            statement.setString(1, customer.getName());
+            statement.setString(2, customer.getUsername());
+            statement.setString(3, customer.getAddress());
+            statement.setString(4, customer.getId());
+
+            return statement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static ArrayList<Customer> getCustomers() {
+        try {
+            String sql = "SELECT * FROM CUSTOMER";
+            PreparedStatement statement  = DatabaseConnection.getConnection().prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+            ArrayList<Customer> list = new ArrayList<Customer>();
+
+            while (result.next()) {
+                Customer customer = new Customer();
+                customer.setId(result.getString("ID"));
+                customer.setName(result.getString("NAME"));
+                customer.setUsername(result.getString("USERNAME"));
+                customer.setAddress(result.getString("ADDRESS"));
+                list.add(customer);
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
