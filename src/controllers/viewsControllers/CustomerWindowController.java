9a7cc492f8e05f73a332a02b57fc0150bd33ee30
Haytham Salama <haythamasalama@gmail.com>
2022-05-22 03:27:17 +0300

    Update CustomerWindowController.java

--- src/controllers/viewsControllers/CustomerWindowController.java
+++ src/controllers/viewsControllers/CustomerWindowController.java
@@ -2,11 +2,20 @@
 package controllers.viewsControllers;
 
 
+import dao.DepartmentDAO;
+import dao.ProductDAO;
+import model.Department;
+import model.Product;
+
+import java.util.ArrayList;
 
 public class CustomerWindowController {
 
-
-    public CustomerWindowController() {
+    public static ArrayList<Department> getDepartments() {
+        return DepartmentDAO.getDepartments();
     }
 
+    public static ArrayList<Product> getProductsByDepartmentID(String departmentID) {
+        return ProductDAO.getProductsByDepartmentID(departmentID);
+    }
 }
