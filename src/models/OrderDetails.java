1b8aa33d9a5d3b404e468f6b9679ee05af67f114
Haytham Salama <haythamasalama@gmail.com>
2022-05-24 06:22:15 +0300

    Create OrderDetails.java

---
+++ src/models/OrderDetails.java
@@ -0,0 +1,60 @@
+package models;
+
+public class OrderDetails {
+
+    private String id;
+    private String orderID;
+    private String productID;
+    private double price;
+    private int quantity;
+
+    public OrderDetails() {
+    }
+
+    public OrderDetails(String orderID, String productID, double price, int quantity) {
+        this.orderID = orderID;
+        this.productID = productID;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
