1eaa92db838e072852b45af01211c1024c3af4cb
Haytham Salama <haythamasalama@gmail.com>
2022-05-24 06:24:57 +0300

    Update CartCheckoutWindowController.java

--- src/controllers/viewsControllers/CartCheckoutWindowController.java
+++ src/controllers/viewsControllers/CartCheckoutWindowController.java
@@ -2,11 +2,14 @@
 package controllers.viewsControllers;
 
 
+import dao.CartDAO;
+import models.Product;
+
+import java.util.ArrayList;
 
 public class CartCheckoutWindowController {
   
-
-    public CartCheckoutWindowController() {
-
-    } 
+    public static ArrayList<Product> getCarBytCustomerId(String customerID) {
+        return CartDAO.getCarBytCustomerId(customerID);
+    }
 }
