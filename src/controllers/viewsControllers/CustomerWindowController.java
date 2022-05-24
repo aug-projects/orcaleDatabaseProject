a7b15407ee875459d511423796c3ec26fae70ae6
Haytham Salama <haythamasalama@gmail.com>
2022-05-24 06:24:50 +0300

    Update CustomerWindowController.java

--- src/controllers/viewsControllers/CustomerWindowController.java
+++ src/controllers/viewsControllers/CustomerWindowController.java
@@ -2,14 +2,19 @@
 package controllers.viewsControllers;
 
 
-import dao.DepartmentDAO;
-import dao.ProductDAO;
-import model.Department;
-import model.Product;
+import dao.*;
+import models.*;
 
 import java.util.ArrayList;
 
 public class CustomerWindowController {
+
+/*
+|--------------------------------------------------------------------------
+| Department
+|--------------------------------------------------------------------------
+|
+*/
 
     public static ArrayList<Department> getDepartments() {
         return DepartmentDAO.getDepartments();
@@ -18,4 +23,59 @@ public class CustomerWindowController {
     public static ArrayList<Product> getProductsByDepartmentID(String departmentID) {
         return ProductDAO.getProductsByDepartmentID(departmentID);
     }
+
+/*
+|--------------------------------------------------------------------------
+| carts
+|--------------------------------------------------------------------------
+|
+*/
+
+    public static String getCartCustomerByProduct(String productId,String customerId) {
+        return CartDAO.getCartCustomerByProduct(productId,customerId);
+    }
+
+    public static boolean insertCart(Cart cart) {
+        return CartDAO.insert(cart);
+    }
+
+    public static void updateCart(Cart cart) {
+        CartDAO.update(cart);
+    }
+
+
+/*
+|--------------------------------------------------------------------------
+| Favourite Product
+|--------------------------------------------------------------------------
+|
+*/
+
+    public static boolean insertFavouriteProduct(FavouriteProduct favouriteProduct) {
+        return  FavouriteProductDAO.insert(favouriteProduct);
+    }
+
+    public static void deleteFavouriteProduct(String productId) {
+        FavouriteProductDAO.delete(productId);
+    }
+
+    public static boolean getFavouriteByProductId(String productId) {
+        return FavouriteProductDAO.getFavouriteByProductId(productId);
+    }
+
+    public static ArrayList<Product> getFavouritesByProductId(String productId) {
+        return FavouriteProductDAO.getFavouritesByProductId(productId);
+    }
+
+/*
+|--------------------------------------------------------------------------
+| Log
+|--------------------------------------------------------------------------
+|
+*/
+
+    public static boolean insertLogs(Log logs) {
+        return LogsDAO.insert(logs);
+    }
+
 }
