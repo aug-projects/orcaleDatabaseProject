6b6b719a560c62dd49f757c720a7a9a9d826c748
Haytham Salama <haythamasalama@gmail.com>
2022-05-24 06:22:17 +0300

    Create Order.java

---
+++ src/models/Order.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Order {
+
+    private String id;
+    private String customerID;
+    private String address;
+    private String description;
+    private double totalPrice;
+
+    public Order() {
+    }
+
+    public Order(String customerID, String address, String description, double totalPrice) {
+        this.customerID = customerID;
+        this.address = address;
+        this.description = description;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
